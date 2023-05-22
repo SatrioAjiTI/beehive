dd0156c62a2f345cb04e54d208b9735e387e32ec
SatrioAjiTI <satrio.aji@ti.ukdw.ac.id>
2023-05-23 01:56:52 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Beranda />
+</template>
+
+<script>
+import BerandaVue from "./components/Beranda.vue";
+export default {
+  name: "App",
+  components: {
+    Beranda: BerandaVue,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,900;1,900&display=swap");
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-family: "Bellota Text";
+  font-style: normal;
+}
+</style>
---
+++ src/components/Beranda.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col-md-12">
+    <div class="background">
+      <div class="row">
+        <div class="col-md-6 ml-auto mr-auto text-center">
+          <h2>Selamat Datang Di</h2>
+          <p>
+            Sistem berbasis cerdas yang dapat memprediksi nilai karbon suatu
+            lahan ataupun area sekitarnya serta mendukung pengambilan data
+            karbon secara otomatis melalui drone.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Bellota+Text&display=swap");
+.background {
+  background: linear-gradient(
+    180deg,
+    rgb(19, 66, 128) 40%,
+    rgba(90.19, 136.15, 196.78, 0.59) 90%,
+    rgba(19, 66, 128, 0.02) 99%,
+    rgba(19, 66, 128, 0) 100%
+  );
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  color: white;
+
+  font-family: "Bellota Text", cursive;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 13px;
+}
+</style>
